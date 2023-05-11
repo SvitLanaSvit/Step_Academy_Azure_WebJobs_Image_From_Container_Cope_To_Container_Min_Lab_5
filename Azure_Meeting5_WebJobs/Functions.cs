7e890c3ae95321a88c19edcb6062efb4926926d8
svetlichnaya.org@gmail.com <svetlichnaya.org@gmail.com>
2023-05-11 15:56:56 +0200

    first commit

---
+++ Azure_Meeting5_WebJobs/Functions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp.Formats.Jpeg;
+
+namespace Azure_Meeting5_WebJobs
+{
+    public static class Functions
+    {
+        public static void ProcessBlobData(
+            [BlobTrigger("images/{filename}")] Stream inputFile,
+            string filename,
+            [Blob("images-min/copy-{filename}", FileAccess.Write)] Stream outputFile,
+            ILogger logger
+            )
+        {
+            using(Image image = Image.Load(inputFile))
+            {
+                if(image != null)
+                {
+                    image.Mutate(x => x.Resize(200, 100));
+                    image.Save(outputFile, new JpegEncoder());
+                }
+            }
+            inputFile.CopyTo(outputFile);
+            logger.LogInformation($"File to process: {filename}");
+            logger.LogInformation($"{inputFile.Length} bytes");
+
+        }
+    }
+}
---
+++ Azure_Meeting5_WebJobs/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+HostBuilder builder = new HostBuilder();
+builder.ConfigureWebJobs(options => {
+    options.AddAzureStorageBlobs();
+    options.AddAzureStorageQueues();
+});
+builder.ConfigureLogging((HostBuilderContext context, ILoggingBuilder logBuilder) => {
+    logBuilder.AddConsole();
+    string appInsightKey = context.Configuration.GetSection("APPINSIGHTS_INSTRUMENTATIONKEY").Value;
+    logBuilder.AddApplicationInsightsWebJobs(config =>
+    {
+        config.InstrumentationKey = appInsightKey;
+    });
+});
+IHost host = builder.Build();
+using (host)
+    host.Run();
